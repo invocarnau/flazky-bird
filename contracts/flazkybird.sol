1eb7d8d3eb67652a13bf5e794bc7a3de7f5240c0
Arnau <arnaubennassar5@gmail.com>
2024-10-13 17:42:06 -0700

    wip

--- contracts/flazkybird.sol
+++ contracts/flazkybird.sol
@@ -1,56 +1,57 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.20;
 
 contract FlazkyBird {
     struct LeaderboardEntry {
         address player;
         uint256 score;
-        address next;
+        uint256 next;
     }
 
-    mapping(address => LeaderboardEntry) public leaderboard;
-    address public leader;
+    mapping(uint256 => LeaderboardEntry) public leaderboard;
+    uint256 public leader;
+    uint256 counter;
 
-    // constructor() public {
-        
-    // }
-
-    function addLeaderboardEntry(uint256 score, address previous) public {
+    function addLeaderboardEntry(uint256 score, uint256 previous) public {
         // TODO: verify ZKP
-        address next;
-        if (previous == address(0)) { // player claims to be the new leader
-            if (leader != address(0)) {
+        counter++;
+        uint256 next;
+        if (previous == 0) { // player claims to be the new leader
+            if (counter != 1) {
                 LeaderboardEntry memory currentLeader = leaderboard[leader];
                 require(currentLeader.score < score, "score should be higher than current lead");
-                next = currentLeader.player;
+                next = leader;
             }
             // update leader, GG
-            leader = msg.sender;
+            leader = counter;
         } else {
             LeaderboardEntry memory previousEntry = leaderboard[previous];
             require(previousEntry.score >= score, "previous entry has lower score");
             next = previousEntry.next;
-            if (next != address(0)) { // player is at the bottom
+            if (next != 0) { // player is at the bottom
                 LeaderboardEntry memory nextEntry = leaderboard[next];
-                require(previousEntry.next == nextEntry.player, "prev/next are inconsistent");
+                require(previousEntry.next == next, "prev/next are inconsistent");
                 require(nextEntry.score < score);
             }
-            previousEntry.next = msg.sender;
-            leaderboard[previousEntry.player] = previousEntry;
+            previousEntry.next = counter;
+            leaderboard[previous] = previousEntry;
         }
 
         // insert score
-        leaderboard[msg.sender] = LeaderboardEntry(msg.sender, score, next);
+        leaderboard[counter] = LeaderboardEntry(msg.sender, score, next);
     }
 
-    function getLeaderboard(address from, uint256 items) public view returns (LeaderboardEntry[] memory) {
+    function getLeaderboard(uint256 from, uint256 items) public view returns (LeaderboardEntry[] memory, uint256 nextIndex) {
         LeaderboardEntry[] memory entries = new LeaderboardEntry[](items);
-        address next = from;
+        uint256 next = from;
+        if (next == 0) {
+            next = leader;
+        }
         for (uint i = 0; i < items; i++) {
             entries[i] = leaderboard[next];
             next = entries[i].next;
         }
-        return entries;
+        return (entries, next);
     }
 }
